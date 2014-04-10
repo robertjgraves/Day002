87d413618f28e113677662a2f0709f5bf737eeb9
Robert Graves <robertjgraves@gmail.com>
2014-04-10 09:13:32 -0700

    string to date to capture birthday

--- Day002/Day002/Program.cs
+++ Day002/Day002/Program.cs
@@ -10,6 +10,16 @@ namespace Day002
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your birthday (mm/dd/yyyy format)");
+            string input = Console.ReadLine();
+            DateTime myBirthday = DateTime.Parse(input);
+
+            Console.WriteLine("Your birthday is {0:d}", myBirthday);
+
+            DateTime today = DateTime.Now;
+
+
+            Console.ReadLine();
         }
     }
 }
